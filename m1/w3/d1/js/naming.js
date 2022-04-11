1ca04e719ba84e08b909ebbe4b01016c02e14d24
MichaelG0 <gcloner@gmail.com>
2022-04-11 15:57:21 +0200

    commit10

---
+++ m1/w3/d1/js/naming.js
@@ -0,0 +1,35 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet + ' ' + _pet2 + ' ' + PetPreferito);
+
+//var 12 = 'numero';
+
+var x = 5
+var y = 10
+document.getElementById('demo').innerHTML = x + y
+
+var z = x + y
+alert(z)
+
+var firstName = 'John', lastName = 'Doe', age = 35;
+
+function myFunction() {
+    alert('Hello World!');
+}
+myFunction()
+
+function myFunction2() {
+    return 'Hello';
+}
+document.getElementById('demo2').innerHTML = myFunction2();
