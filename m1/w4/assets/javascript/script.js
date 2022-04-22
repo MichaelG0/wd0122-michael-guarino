09c92bfa54c72d38f61ca1ea54a12836dca04ecb
MichaelG0 <gcloner@gmail.com>
2022-04-22 16:24:54 +0200

    commit15

--- m1/w4/assets/javascript/script.js
+++ m1/w4/assets/javascript/script.js
@@ -43,19 +43,46 @@ info2.addEventListener('mouseleave',function(e2){
 const next = document.querySelector('.avanti')
 
 
-let img = document.querySelector('.scroller')
+var img = document.querySelector('.scroller')
 
 let n = 0
 
-next.addEventListener('click', function(e){
-    n++
+next.addEventListener('click', function (e){
+    var x = window.matchMedia("(max-width: 767px)")
+    if (x.matches) {
+        n = n + 2;
+    }else{
+        n++
+    }
     if(n > 4){
         n = 0
     }
-    let p = -25 * n;
+    let p = -25.5 * n;
     img.style.transform = 'translateX(' + p + '%)'
-    img.style.transition = "all 2s";
+
+    if(n == 4){
+        pavanti.style.color = 'red'
+        pindietro.style.color = 'rgb(212, 209, 209)'
+    }else{
+        pindietro.style.color = 'red'
+        pavanti.style.color = 'rgb(212, 209, 209)'
+    }
 })
+
+
+// function myFunction(x) {
+//     if (x.matches) {
+//     n = n*2
+//     } else {
+//     //  n=0
+//     }
+//   }
+  
+//   myFunction(x)
+//   x.addListener(myFunction)
+
+
+
 
 
 // nav bar
@@ -72,48 +99,69 @@ window.addEventListener('scroll',function(e) {
     let s5 = document.querySelector('#news').offsetTop - 250
     
     let s6 = document.querySelector('#contatti').offsetTop - 250
-
-
+    
+    
     if(s < s2){
-        this.document.querySelectorAll('#navdesktop li')[0].style.textDecoration = 'underline'
-        this.document.querySelectorAll('#navmobile li')[0].style.textDecoration = 'underline'
+        this.document.querySelectorAll('#navdesktop li')[0].style.borderBottom = 'solid rgb(255, 72, 0)'
+        this.document.querySelectorAll('#navmobile li')[0].style.borderBottom = 'solid rgb(255, 72, 0)'
     }else{
-        this.document.querySelectorAll('#navdesktop li')[0].style.textDecoration = 'none'
-        this.document.querySelectorAll('#navmobile li')[0].style.textDecoration = 'none'
+        this.document.querySelectorAll('#navdesktop li')[0].style.borderBottom = 'none'
+        this.document.querySelectorAll('#navmobile li')[0].style.borderBottom = 'none'
     }
     if(s >= s2 && s < s3){
-        this.document.querySelectorAll('#navdesktop li')[1].style.textDecoration = 'underline'
-        this.document.querySelectorAll('#navmobile li')[1].style.textDecoration = 'underline'
+        this.document.querySelectorAll('#navdesktop li')[1].style.borderBottom = 'solid rgb(255, 72, 0)'
+        this.document.querySelectorAll('#navmobile li')[1].style.borderBottom = 'solid rgb(255, 72, 0)'
     }else{
-        this.document.querySelectorAll('#navdesktop li')[1].style.textDecoration = 'none'
-        this.document.querySelectorAll('#navmobile li')[1].style.textDecoration = 'none'
+        this.document.querySelectorAll('#navdesktop li')[1].style.borderBottom = 'none'
+        this.document.querySelectorAll('#navmobile li')[1].style.borderBottom = 'none'
     }
     if(s >= s3 && s < s4){
-        this.document.querySelectorAll('#navdesktop li')[2].style.textDecoration = 'underline'
-        this.document.querySelectorAll('#navmobile li')[2].style.textDecoration = 'underline'
+        this.document.querySelectorAll('#navdesktop li')[2].style.borderBottom = 'solid rgb(255, 72, 0)'
+        this.document.querySelectorAll('#navmobile li')[2].style.borderBottom = 'solid rgb(255, 72, 0)'
     }else{
-        this.document.querySelectorAll('#navdesktop li')[2].style.textDecoration = 'none'
-        this.document.querySelectorAll('#navmobile li')[2].style.textDecoration = 'none'
+        this.document.querySelectorAll('#navdesktop li')[2].style.borderBottom = 'none'
+        this.document.querySelectorAll('#navmobile li')[2].style.borderBottom = 'none'
     }
     if(s >= s4 && s < s5){
-        this.document.querySelectorAll('#navdesktop li')[3].style.textDecoration = 'underline'
-        this.document.querySelectorAll('#navmobile li')[3].style.textDecoration = 'underline'
+        this.document.querySelectorAll('#navdesktop li')[3].style.borderBottom = 'solid rgb(255, 72, 0)'
+        this.document.querySelectorAll('#navmobile li')[3].style.borderBottom = 'solid rgb(255, 72, 0)'
     }else{
-        this.document.querySelectorAll('#navdesktop li')[3].style.textDecoration = 'none'
-        this.document.querySelectorAll('#navmobile li')[3].style.textDecoration = 'none'
+        this.document.querySelectorAll('#navdesktop li')[3].style.borderBottom = 'none'
+        this.document.querySelectorAll('#navmobile li')[3].style.borderBottom = 'none'
     }
     if(s >= s5 && s < s6){
-        this.document.querySelectorAll('#navdesktop li')[4].style.textDecoration = 'underline'
-        this.document.querySelectorAll('#navmobile li')[4].style.textDecoration = 'underline'
+        this.document.querySelectorAll('#navdesktop li')[4].style.borderBottom = 'solid rgb(255, 72, 0)'
+        this.document.querySelectorAll('#navmobile li')[4].style.borderBottom = 'solid rgb(255, 72, 0)'
     }else{
-        this.document.querySelectorAll('#navdesktop li')[4].style.textDecoration = 'none'
-        this.document.querySelectorAll('#navmobile li')[4].style.textDecoration = 'none'
+        this.document.querySelectorAll('#navdesktop li')[4].style.borderBottom = 'none'
+        this.document.querySelectorAll('#navmobile li')[4].style.borderBottom = 'none'
     }
     if(s >= s6){
-        this.document.querySelectorAll('#navdesktop li')[5].style.textDecoration = 'underline'
-        this.document.querySelectorAll('#navmobile li')[5].style.textDecoration = 'underline'
+        this.document.querySelectorAll('#navdesktop li')[5].style.borderBottom = 'solid rgb(255, 72, 0)'
+        this.document.querySelectorAll('#navmobile li')[5].style.borderBottom = 'solid rgb(255, 72, 0)'
     }else{
-        this.document.querySelectorAll('#navdesktop li')[5].style.textDecoration = 'none'
-        this.document.querySelectorAll('#navmobile li')[5].style.textDecoration = 'none'
+        this.document.querySelectorAll('#navdesktop li')[5].style.borderBottom = 'none'
+        this.document.querySelectorAll('#navmobile li')[5].style.borderBottom = 'none'
     }
+})
+
+// punti
+
+let pavanti = document.querySelector('.pavanti')
+
+let pindietro = document.querySelector('.pindietro')
+
+
+pavanti.addEventListener('click',function(e){
+    img.style.transform = 'translateX(-102%)'
+    pavanti.style.color = 'red'
+    pindietro.style.color = 'rgb(212, 209, 209)'
+    n = 4
+})
+
+pindietro.addEventListener('click',function(e){
+    img.style.transform = 'translateX(0%)'
+    pindietro.style.color = 'red'
+    pavanti.style.color = 'rgb(212, 209, 209)'
+    n = 0
 })
